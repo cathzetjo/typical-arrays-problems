95dcdfd66c151bd59c3c2bf186c75ec9a663cc10
Katerina Zhadko <katerinkaj@icloud.com>
2021-08-23 21:51:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array && array.length) {
+        return Math.min.apply(Math, array);
+    }
+    return 0;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array && array.length) {
+        return Math.max.apply(Math, array);
+    }
+    return 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+        if (array && array.length) {
+        const average = (array) => array.reduce((a, b) => a + b) / array.length;
+        return average(array);
+    }
+    return 0;
+};
